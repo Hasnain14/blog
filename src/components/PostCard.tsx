3d7ac38f3fd93d49dbbfeb8ecf9a117a3f171bed
Hasnain14 <hasnainmahmud14@gmail.com>
2025-02-01 22:44:28 +0600

    bug fixed

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,11 +1,11 @@
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import ShareIcon from '@mui/icons-material/Share';
@@ -13,9 +13,9 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Checkbox } from '@mui/material';
 import { Favorite, FavoriteBorder } from '@mui/icons-material';
 
-interface ExpandMoreProps extends IconButtonProps {
-    expand: boolean;
-}
+// interface ExpandMoreProps extends IconButtonProps {
+//     expand: boolean;
+// }
 
 interface Post {
     userId: number;
@@ -25,29 +25,29 @@ interface Post {
 }
 
 
-const ExpandMore = styled((props: ExpandMoreProps) => {
-    const { ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme }) => ({
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-    variants: [
-        {
-            props: ({ expand }) => !expand,
-            style: {
-                transform: 'rotate(0deg)',
-            },
-        },
-        {
-            props: ({ expand }) => !!expand,
-            style: {
-                transform: 'rotate(180deg)',
-            },
-        },
-    ],
-}));
+// const ExpandMore = styled((props: ExpandMoreProps) => {
+//     const { ...other } = props;
+//     return <IconButton {...other} />;
+// })(({ theme }) => ({
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//         duration: theme.transitions.duration.shortest,
+//     }),
+//     variants: [
+//         {
+//             props: ({ expand }) => !expand,
+//             style: {
+//                 transform: 'rotate(0deg)',
+//             },
+//         },
+//         {
+//             props: ({ expand }) => !!expand,
+//             style: {
+//                 transform: 'rotate(180deg)',
+//             },
+//         },
+//     ],
+// }));
 
 export default function PostCard({ postTitle, postBody }: Post) {
 
